057e1d15e929c6107a5f22b87d2e26f22f008ede
leticia.orive <leticia.orive@accenture.com>
2026-04-15 09:05:56 +0200

    creacion de un models con su interface

---
+++ proyecto-angular/src/app/models/pais-interface.ts
@@ -0,0 +1,30 @@
+/**
+ * Modelo de datos para representar un pais en la aplicacion.
+ *
+ * Para que sirve:
+ * - Define la estructura que deben tener los datos de un pais.
+ * - Ayuda a TypeScript a validar tipos y evita errores al usar la respuesta de una API.
+ * - Mejora autocompletado y mantenimiento del codigo.
+ *
+ * Como se utiliza:
+ * 1) Importa la interfaz donde la necesites.
+ *    import { Pais } from './models/pais-interface';
+ *
+ * 2) Tipa variables, parametros o respuestas HTTP.
+ *    paises: Pais[] = [];
+ *
+ * 3) Accede a propiedades con seguridad de tipos.
+ *    const nombre = pais.name.common;
+ *    const capital = pais.capital[0];
+ */
+export interface Pais {
+    name: {
+        common : string;
+    }
+    capital: string[];
+    population: number;
+    flags: {
+        png : string;
+        svg: string
+    }
+}
